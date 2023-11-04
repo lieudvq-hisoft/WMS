fa64e96b5860f856ff73f1866b3c8612be7e1261
longnn26 <longnn2605@gmail.com>
2023-11-04 11:52:24 +0700

    Migration

---
+++ WMS/Migrations/20231104045158_entityPickingRequestEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class entityPickingRequestEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PickingRequestInventory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PickingRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InventoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickingRequestInventory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PickingRequestInventory_Inventory_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PickingRequestInventory_PickingRequest_PickingRequestId",
+                        column: x => x.PickingRequestId,
+                        principalTable: "PickingRequest",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickingRequestInventory_InventoryId",
+                table: "PickingRequestInventory",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickingRequestInventory_PickingRequestId",
+                table: "PickingRequestInventory",
+                column: "PickingRequestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PickingRequestInventory");
+        }
+    }
+}
