700d92ece3e9052e214118700958ab9f9f76c7ca
longnn26 <longnn2605@gmail.com>
2023-11-08 18:03:16 +0700

    Update StartupExtension.cs

--- WMS/Extensions/StartupExtension.cs
+++ WMS/Extensions/StartupExtension.cs
@@ -157,7 +157,7 @@ public static class StartupExtension
                     },
                     OnMessageReceived = context =>
                     {
-                        var accessToken = context.Request.Query["access_token"];
+                        var accessToken = context.Request.Query["accessToken"];
 
                         // If the request is for our hub...
                         var path = context.HttpContext.Request.Path;
