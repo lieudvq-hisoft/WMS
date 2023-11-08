cebe6d078d3e3fb4314790cbd65427cea6edf765
lieudvq-hisoft <lieudvq@hisoft.vn>
2023-11-08 18:14:20 +0700

    Update StartupExtension.cs

--- WMS/Extensions/StartupExtension.cs
+++ WMS/Extensions/StartupExtension.cs
@@ -157,7 +157,7 @@ public static class StartupExtension
                     },
                     OnMessageReceived = context =>
                     {
-                        var accessToken = context.Request.Query["accessToken"];
+                        var accessToken = context.Request.Query["access_token"];
 
                         // If the request is for our hub...
                         var path = context.HttpContext.Request.Path;
