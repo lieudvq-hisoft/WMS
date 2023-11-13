ff4ace8f0e8e5ec30adf5012c2d38022b7267d1d
longnn26 <longnn2605@gmail.com>
2023-11-13 11:26:06 +0700

    Feat(@user): return thêm filed đầy đủ của user như : fristname, lastname ,...,  và Role

--- Data/Models/Token.cs
+++ Data/Models/Token.cs
@@ -15,6 +15,9 @@ public class Token
     public int ExpiresIn { get; set; }
     public string UserName { get; set; }
     public string PhoneNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string UserAva { get; set; }
     public int CurrenNoticeCount { get; set; }
+    public List<string> Roles { get; set; } = new List<string>();
 }
