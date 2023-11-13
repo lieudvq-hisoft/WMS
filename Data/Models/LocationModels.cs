17b022ff4d1e16c84452b40c1bec170164254b48
lieudvq-hisoft <lieudvq@hisoft.vn>
2023-11-13 11:20:04 +0700

    Feat(@location): return thêm object Product{  id , ....}

--- Data/Models/LocationModels.cs
+++ Data/Models/LocationModels.cs
@@ -11,6 +11,19 @@ namespace Data.Models
         public int SectionNumber { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
+
+        public List<InventoryForLocation> Inventories { get; set; }
+    }
+
+    public class InventoryForLocation
+    {
+        public Guid Id { get; set; }
+        public string Note { get; set; }
+        public int QuantityOnHand { get; set; } = 0;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public virtual ProductModel Product { get; set; }
+
     }
 
     public class LocationCreateModel
